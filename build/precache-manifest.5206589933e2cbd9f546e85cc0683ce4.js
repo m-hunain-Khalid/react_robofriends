1b0edc591bbd8accae25d705edc8c044862fdbf4
hunain <mhunain@csquareonline.com>
2019-10-01 18:22:49 +0500

    my first react

---
+++ build/precache-manifest.5206589933e2cbd9f546e85cc0683ce4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0259258d417424f6273c7da817971deb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7dd4269bb9399ce919aa",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "ef2b107dc10baae6164b",
+    "url": "/static/css/main.bfc48a56.chunk.css"
+  },
+  {
+    "revision": "7dd4269bb9399ce919aa",
+    "url": "/static/js/2.0a6ad8b6.chunk.js"
+  },
+  {
+    "revision": "ef2b107dc10baae6164b",
+    "url": "/static/js/main.4d033a51.chunk.js"
+  },
+  {
+    "revision": "720e22d81d6d677b3a62",
+    "url": "/static/js/runtime-main.c26e8b41.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
